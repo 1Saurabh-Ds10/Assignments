77d5236d70feaf255839d27e968103ed9b1cc3c8
saurabh010 <1saurabh.desai@gmail.com>
2015-09-18 12:40:57 +0530

    Update script2.js

--- Part-1/JS/script2.js
+++ Part-1/JS/script2.js
@@ -4,7 +4,7 @@
 		2) Newly entered unique values are not getting concatenated with existing predefined Array.
 		   Eg: If the predefined array has values like 1000, 1001 & if the user enters 1002 then 1002 will not get concatenated
 		       with existing 1000, 1001. So in the next iteration if the user again enters 1002, it will display the same result
-               as before.	
+               	       as before.	
 		3) This program will throw an error for improper range.
 
 */
@@ -83,7 +83,6 @@
 		   It takes the values entered in the textbox by the user, passes & calls 'getDuplicateAndUniqueArrayValues'
 		   function of logic Component(where all the processing gets performed).
 		   And finally sets State with the values returned from the above function.
-		
 		*/
 		handleKeyUp: function(e) {
 			
@@ -164,7 +163,7 @@
 			});
 			
 			/* 'allUniqueValsArr' array is the combination of predefined array & unique user entered elements,
-				which were not there in predefined array.
+			    which were not there in predefined array.
 			*/
 			objWrapper.allUniqueValsArr = objWrapper.allUniqueValsArr.concat(arrayToCheck, objWrapper.uniqueValsArr);
 			return objWrapper;
@@ -198,7 +197,7 @@
 				} else {
 				
 					var range1 = Number(element.split('-')[0]),
-						range2 = Number(element.split('-')[1]);
+			  		    range2 = Number(element.split('-')[1]);
 						
 					if (!(isNaN(range1)) && !(isNaN(range2))) {
 						
